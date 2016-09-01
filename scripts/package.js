d89c65639b4cdb358add8de7e1bcc40734a45ab6
Vu Tran <vu.tran@thermofisher.com>
2016-09-01 13:49:14 -0700

    updated package scripts

--- scripts/package.js
+++ scripts/package.js
@@ -2,16 +2,47 @@ const path = require('path');
 const packager = require('electron-packager');
 const chalk = require('chalk');
 
-const options = {
+const DEFAULT_OPTIONS = {
   name: 'Dext',
   dir: path.resolve(__dirname, '..'),
   out: './dist',
-  arch: 'all',
-  platform: 'darwin',
   asar: true,
   overwrite: true,
+  ignore: [
+    '.DS_Store',
+  ],
 };
 
-packager(options, (err, paths) => {
-  console.log(chalk.green('Packaging complete.'));
+/**
+ * Packages the app for the given platform
+ *
+ * @param {String} platform
+ * @param {String} arch
+ */
+const pkg = (platform, arch) => new Promise(resolve => {
+  const opts = Object.assign({}, DEFAULT_OPTIONS, {
+    platform,
+    arch,
+  });
+  packager(opts, err => {
+    resolve({ err, options: { platform, arch } });
+  });
 });
+
+/**
+ * Print the status of the package
+ *
+ * @param {String} err
+ */
+const printStatus = ({ err, options }) => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.log(chalk.red(`${options.platform}-${options.arch} packaging error.`));
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(chalk.green(`${options.platform}-${options.arch} package complete.`));
+  }
+};
+
+// start packaging
+pkg('darwin', 'x64').then(printStatus);
