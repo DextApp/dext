225f4fd4c5c6f5ab97e528744ec3f6a3df295541
Vu Tran <vu@vu-tran.com>
2016-09-12 21:18:24 -0700

    use screen-saver pkg

--- resources/plugins/screensaver/start.js
+++ resources/plugins/screensaver/start.js
@@ -1,9 +1,3 @@
-const { exec, execFile } = require('child_process');
+const screenSaver = require('screen-saver');
 
-if (process.platform === 'darwin') {
-  execFile('open', ['/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app']);
-} else if (process.platform === 'win32') {
-  execFile('nircmd.exe', ['screensaver']);
-} else if (process.platform === 'linux') {
-  exec('xdg-screensaver', ['activate']);
-}
+screenSaver();
