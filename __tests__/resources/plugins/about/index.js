3a35b58f8e68c3667aecc7c1480a98c27901cc4c
Adnan Asani <konjuku@gmail.com>
2016-10-03 23:53:34 +0200

    tests(unit-test): Fix the unit-test

--- __tests__/resources/plugins/about/index.js
+++ __tests__/resources/plugins/about/index.js
@@ -2,12 +2,12 @@ import m from '../../../../resources/plugins/about';
 
 describe('calculator', () => {
   it('should return something', () => {
-    const results = m.execute('?');
+    const results = m.query('?');
     expect(results.items.length).toBeGreaterThan(0);
   });
 
   it('should return nothing', () => {
-    const results = m.execute('');
+    const results = m.query('');
     expect(results.items.length).not.toBeGreaterThan(0);
   });
 });
