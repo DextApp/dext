987663ad698c6dc720faa27ccac396ffb3b2731c
Vu Tran <vu@vu-tran.com>
2016-10-09 06:03:10 -0700

    add fs mock (#95)

---
+++ __mocks__/fs.js
@@ -0,0 +1,48 @@
+const fs = {};
+
+let mockFiles = [];
+let mockFileData = '';
+
+/**
+ * Sets the mock return value for fs.readdir
+ *
+ * @param {String[]} files - An array of files
+ */
+ // eslint-disable-next-line no-underscore-dangle
+fs.__setFiles = (files) => {
+  mockFiles = files;
+};
+
+/**
+ * Sets the file data for fs.readFile
+ *
+ * @param {String} data
+ */
+// eslint-disable-next-line no-underscore-dangle
+fs.__setFileData = (data) => {
+  mockFileData = data;
+};
+
+/**
+ * Reads the directory and apply the callback
+ * with the mocked values
+ *
+ * @param {String} directory
+ * @param {Function} callback
+ */
+fs.readdir = (directory, callback) => {
+  callback.call(null, null, mockFiles);
+};
+
+/**
+ * Reads the file and apply the callback
+ * with the mocked data
+ *
+ * @param {String} file
+ * @param {Function} callback
+ */
+fs.readFile = (file, callback) => {
+  callback.call(null, null, mockFileData);
+};
+
+module.exports = fs;
