b7660cbba6d60939df84bac5a3a04e7ded868211
Vu Tran <vu@vu-tran.com>
2016-10-12 00:37:06 -0700

    fixed mods bug (#105)
    * fixed mods bug
    
    * fix loading non-existent theme
    
    * reset keys on window show

--- app/renderer/src/actions/creators.js
+++ app/renderer/src/actions/creators.js
@@ -14,6 +14,7 @@ import {
   OPEN_DETAILS,
   SET_ACTIVE_KEY,
   CLEAR_ACTIVE_KEY,
+  RESET_KEYS,
 } from './types';
 
 /**
@@ -157,4 +158,14 @@ export function setActiveKey(key) {
  */
 export function clearActiveKey(key) {
   return { type: CLEAR_ACTIVE_KEY, key };
+}
+
+/**
+ * Resets all keys
+ *
+ * @param {String} key
+ * @return {Object}
+ */
+export function resetKeys() {
+  return { type: RESET_KEYS };
 }
