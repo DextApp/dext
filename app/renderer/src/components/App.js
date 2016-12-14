04b39496fc6133f0d26b5d6982e5fcd3b135d8d1
Vu Tran <vu@vu-tran.com>
2016-12-14 00:46:54 -0800

    split outer and inner styles

--- app/renderer/src/components/App.js
+++ app/renderer/src/components/App.js
@@ -4,6 +4,13 @@ import QueryFieldContainer from '../containers/QueryFieldContainer';
 import ResultListContainer from '../containers/ResultListContainer';
 import { ThemeSchema } from '../schema';
 
+const outerStyles = style({
+  boxSizing: 'border-box',
+  width: '100%',
+  paddingLeft: 3,
+  paddingRight: 3,
+});
+
 const base = style({
   backgroundColor: '#f2f2f2',
   color: '#333',
@@ -12,21 +19,22 @@ const base = style({
   boxSizing: 'border-box',
   fontFamily: 'Lucida Grande, Arial, sans-serif',
   fontWeight: 'lighter',
-  padding: 0,
-  width: '99%',
-  marginLeft: '3px',
+  width: '100%',
   overflow: 'hidden',
 });
 
 const App = ({ theme }) => {
-  const styles = theme.window
+
+  const innerStyles = theme.window
     ? compose(base, theme.window)
     : base;
 
   return (
-    <div {...styles}>
-      <QueryFieldContainer theme={theme} />
-      <ResultListContainer theme={theme} />
+    <div {...outerStyles}>
+      <div {...innerStyles}>
+        <QueryFieldContainer theme={theme} />
+        <ResultListContainer theme={theme} />
+      </div>
     </div>
   );
 };
