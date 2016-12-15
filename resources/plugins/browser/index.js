73a6a914cf6aca5a1a9d803be0f4783653005245
Vu Tran <vutran@dropbox.com>
2016-12-14 18:27:16 -0800

    add browser plugin

---
+++ resources/plugins/browser/index.js
@@ -0,0 +1,35 @@
+const IS_URL = /(?:https?:\/\/)?([\w\d-]+(?:\.[\w]+)+$)/;
+
+/**
+ * Returns the normalized URL (with protocol scheme)
+ *
+ * @param {String} url
+ * @return {String}
+ */
+const normalize = (url) => {
+  console.log('test', url);
+  if (/^https?:\/\//.test(url)) {
+    return url;
+  }
+  return `https://${url}`;
+};
+
+module.exports = {
+  action: 'openurl',
+  query: (query) => {
+    const items = [];
+    const urls = IS_URL.exec(query);
+    if (urls) {
+      items.push({
+        title: `Open ${query} in the browser.`,
+        browser: 'Open in browser',
+        arg: normalize(query),
+        icon: {
+          type: 'text',
+          letter: urls[1].substr(0, 1).toUpperCase(),
+        },
+      });
+    }
+    return { items };
+  },
+};
