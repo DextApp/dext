64280b75582a87bb11f906df342d53ca82721e89
Vu Tran <vu@vu-tran.com>
2016-12-22 21:37:41 -0800

    Added test

---
+++ __tests__/resources/plugins/apps/index.js
@@ -0,0 +1,38 @@
+import m from '../../../../resources/plugins/apps';
+
+jest.mock('fs');
+
+describe('apps', () => {
+  it('should return something', async () => {
+    require('fs').__setFiles([
+      'Safari.app',
+      'Google Chrome.app',
+      'Firefox.app',
+    ]);
+    const results = await m.query('Safari');
+    expect(results.items.length).toBe(1);
+    expect(results.items).toContainEqual({
+      title: 'Safari',
+      subtitle: '/Applications/Safari.app',
+      arg: '/Applications/Safari.app',
+      icon: {
+        type: 'text',
+        letter: 'S',
+      },
+    });
+    expect(results.items).not.toContainEqual({
+      title: 'Google Chrome',
+      subtitle: '/Applications/Google Chrome.app',
+      arg: '/Applications/Google Chrome.app',
+      icon: {
+        type: 'text',
+        letter: 'G',
+      },
+    });
+  });
+
+  it('should return nothing', async () => {
+    const results = await m.query('');
+    expect(results.items.length).not.toBeGreaterThan(0);
+  });
+});
