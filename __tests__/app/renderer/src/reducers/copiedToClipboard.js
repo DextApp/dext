3e87bc9642e400a4254467ecbb52fea831de99d1
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-23 11:28:29 -0500

    Actually I figured the ONLY action that should set the copiedToClipboard state to true is copy item, all of the rest of ‘em should set it to false. Which makes sense? Also, if we add an action that should set it to true, the test will fail and makes use adding it to the test which is good.

---
+++ __tests__/app/renderer/src/reducers/copiedToClipboard.js
@@ -0,0 +1,27 @@
+import * as types from '../../../../../app/renderer/src/actions/types';
+import copiedToClipboard from '../../../../../app/renderer/src/reducers/copiedToClipboard';
+
+const { COPY_ITEM, ...allTypes } = types;
+
+describe('copiedToClipboard reducer', () => {
+  it('should return the initial state', () => {
+    const state = undefined;
+    const action = { type: 'SOME_RANDOM_ACTION' };
+
+    expect(copiedToClipboard(state, action)).toBe(false);
+  });
+
+  it('should handle COPY_ITEM action', () => {
+    const state = undefined;
+    const action = { type: COPY_ITEM };
+
+    expect(copiedToClipboard(state, action)).toBe(true);
+  });
+
+  it('Should handle the rest of the actions', () => {
+    const state = undefined;
+    Object.values(allTypes).map(type => (
+      expect(copiedToClipboard(state, { type })).toBe(false)
+    ));
+  });
+});
--- app/renderer/src/reducers/copiedToClipboard.js
+++ app/renderer/src/reducers/copiedToClipboard.js
@@ -1,21 +1,12 @@
 import {
-  UPDATE_QUERY,
-  SELECT_ITEM,
-  SELECT_NEXT_ITEM,
-  SELECT_PREVIOUS_ITEM,
   COPY_ITEM,
 } from '../actions/types';
 
-export default function (state = false, { type }) {
+export default function (state, { type }) {
   switch (type) {
-    case UPDATE_QUERY:
-    case SELECT_ITEM:
-    case SELECT_NEXT_ITEM:
-    case SELECT_PREVIOUS_ITEM:
-      return false;
     case COPY_ITEM:
       return true;
     default:
-      return state;
+      return false;
   }
 }
