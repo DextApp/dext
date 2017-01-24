f311912013294ecccc9cbc49ae697c66ba391d0d
Thomas Lefebvre <Magellol@users.noreply.github.com>
2017-01-24 17:21:15 -0500

    Lower debounce (#158)
    * lower debounce to 150ms
    
    * Lowering the debounce time for query. That'll feel much faster when typing.

--- app/constants.js
+++ app/constants.js
@@ -4,4 +4,5 @@ module.exports = {
   MAX_RESULTS: 20,
   IS_DEV: process.env.NODE_ENV === 'development',
   CORE_PLUGIN_PATH: path.resolve(__dirname, '..', 'resources', 'plugins'),
+  DEBOUNCE_TIME: 150, // in milliseconds
 };
