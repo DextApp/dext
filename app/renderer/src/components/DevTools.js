934bce77a576e0984bcf83a949664b32e8e3f897
Adnan Asani <adnasa@users.noreply.github.com>
2018-04-02 17:08:12 +0200

    refactor(dev-tools): simplify the rendering (#182)

--- app/renderer/src/components/DevTools.js
+++ app/renderer/src/components/DevTools.js
@@ -3,7 +3,6 @@ import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
-// create the Redux DevTools component
 const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q" defaultIsVisible={false}>
     <LogMonitor theme="solarized" />
