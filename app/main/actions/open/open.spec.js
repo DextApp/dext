1b95b7354180df3ad1bd713f322de4d4005ade85
Adnan Asani <konjuku@gmail.com>
2018-04-14 01:02:32 +0200

    refactor(app/main): move `open` test

---
+++ app/main/actions/open/open.spec.js
@@ -0,0 +1,40 @@
+import open from './open';
+import { shell } from 'electron';
+
+jest.mock('electron', () => ({
+  shell: {
+    openItem: jest.fn(),
+  },
+}));
+
+describe('app/main/actions/open', () => {
+  let message;
+  let arg;
+  beforeEach(() => {
+    message = 'foo';
+    arg = 'bar';
+  });
+  describe('with arg', () => {
+    beforeEach(() => {
+      open(message, arg);
+    });
+    it('should call `shell.openItem`', () => {
+      expect(shell.openItem).toHaveBeenCalledTimes(1);
+    });
+    it('should call `shell.openItem` with `bar', () => {
+      expect(shell.openItem).toHaveBeenCalledWith('bar');
+    });
+  });
+  describe('without arg', () => {
+    beforeEach(() => {
+      arg = undefined;
+      open(message, arg);
+    });
+    it('should not call `shell.openItem`', () => {
+      expect(shell.openItem).not.toHaveBeenCalledTimes(1);
+    });
+  });
+  afterEach(() => {
+    shell.openItem.mockClear();
+  });
+});
