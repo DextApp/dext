332acc3bccd8987dac3f548d54e3a26a06d4a7b3
Adnan Asani <konjuku@gmail.com>
2018-04-14 21:05:46 +0200

    fix(app/renderer): add icon

---
+++ app/renderer/src/components/icon/icon.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Icon from './icon';
+
+describe('rendering', () => {
+  it('should render a text icon component', () => {
+    const icon = {
+      type: 'text',
+      letter: 'T',
+    };
+    const component = renderer.create(<Icon icon={icon} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render a file icon component', () => {
+    const icon = {
+      type: 'file',
+      letter: './icon.png',
+    };
+    const component = renderer.create(<Icon icon={icon} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
