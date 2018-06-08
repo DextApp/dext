0ca7b253329dfda64880ea2830695261c1893c64
Adnan Asani <adnasa@users.noreply.github.com>
2018-06-08 20:09:34 +0200

    test(query-field): use `props`

--- app/renderer/src/components/query-field/query-field.spec.js
+++ app/renderer/src/components/query-field/query-field.spec.js
@@ -2,23 +2,33 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import QueryField from './query-field';
 
+jest.mock('electron', () => ({
+  ipcRenderer: {
+    on: jest.fn(),
+  },
+}));
+
+const createTestProps = custom => ({
+  onReset: jest.fn(),
+  onChange: jest.fn(),
+  ...custom,
+});
+
 describe('<QueryField /> component', () => {
-  let theme = {};
+  let props;
 
   beforeEach(() => {
-    theme = {};
+    props = createTestProps();
   });
 
   it('should render a query field component with no value', () => {
-    const component = renderer.create(<QueryField theme={theme} />);
+    const component = renderer.create(<QueryField {...props} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('should render a query field component with some value', () => {
-    const component = renderer.create(
-      <QueryField theme={theme} value={'TEST'} />
-    );
+    const component = renderer.create(<QueryField {...props} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
